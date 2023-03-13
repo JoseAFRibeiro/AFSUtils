31b2945daa990d512e15491976d9d49d6cc83deb
Laptop Jose <josealbertoribeiro800@gmail.com>
2023-03-13 13:47:09 +0000

    afs struct reading

--- src/afs.c
+++ src/afs.c
@@ -1,9 +1,12 @@
 #include "afsio.h"
 #include "AFS.h"
 
-static inline int readHeader(FILE *f, struct afs_archive *archive)
+static inline int readHeader(FILE *f)
 {
     unsigned char header[4];
+
+    if(f == NULL)
+        return 1;
     
     fread(header, sizeof(char), AFS_SIZE_DWORD, f);
     fseeko64(f, AFS_SIZE_DWORD, SEEK_SET);
@@ -15,15 +18,32 @@ static inline int readHeader(FILE *f, struct afs_archive *archive)
     
     return 0;
 }
-static inline int indexFiles();
+static inline int indexFiles(FILE *f, struct afs_archive *archive)
+{
+    int result = 0;
+
+    fread(&archive->numFiles, sizeof(int), 1, f);
+    archive->entryInfo = malloc(sizeof(struct afs_entry) * archive->numFiles);
+    
+   for(int i = 0; i< archive->numFiles; i++)
+        fread(&archive->entryInfo[i], AFS_ARCHIVE_STRUCT_SIZE, 1, f);
+
+    return 0;
+}
 
 int readAFSFile(const char *inputPath, const char *outputPath)
 {
     int fileSize;
+    int result;
     struct afs_archive archive;
 
     FILE *f = readFile(inputPath, &fileSize);
-    readHeader(f, &archive);
+
+    result = readHeader(f);
+    if(result != 0)
+        return -1;
+
+    result = indexFiles(f, &archive);
 
     return 0;
 }
